e472f20b32b8debdae151e7d1c8ff796cfba6be1
n1kko777 <3734489@gmail.com>
2021-08-08 11:51:26 +0500

    ###

--- bot.js
+++ bot.js
@@ -32,35 +32,6 @@ const setup = (db) => {
   // actions
   bot.action("addBinancePool", addBinancePool);
 
-  //   bot.command("/increment", async (ctx) => {
-  //     const count = (ctx.session.count || 0) + 1;
-
-  //     await ctx.reply(`Count: ${count}`);
-
-  //     ctx.session.count = count;
-  //   });
-
-  //   bot.command("/callback", async (ctx) => {
-  //     await ctx.reply("Inline keyboard with callback", {
-  //       reply_markup: {
-  //         inline_keyboard: [
-  //           [{ text: "Increment", callback_data: "increment_counter" }],
-  //         ],
-  //       },
-  //     });
-  //   });
-
-  //   bot.on("callback_query", async (ctx) => {
-  //     const count = (ctx.session.count || 0) + 1;
-
-  //     // editMessageText
-  //     // console.log(`Callback Query ${ctx.from.id}: ${count}`);
-
-  //     await ctx.answerCbQuery();
-
-  //     ctx.session.count = count;
-  //   });
-
   return bot;
 };
 
