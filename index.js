d93a08500a3babec6d3f7d16999c43da8bbd2318
n1kko777 <3734489@gmail.com>
2021-08-09 11:22:49 +0500

    webhook

--- index.js
+++ index.js
@@ -3,16 +3,26 @@ require("dotenv").config();
 const { MongoClient } = require("mongodb");
 const { setup } = require("./bot");
 
+const URL = process.env.URL || "";
+const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI;
+const BOT_TOKEN = process.env.BOT_TOKEN;
+
 const initialize = async () => {
   const db = (
-    await MongoClient.connect(process.env.MONGODB_URI, {
+    await MongoClient.connect(MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
   ).db();
   const bot = setup(db);
 
-  bot.launch();
+  bot.launch({
+    webhook: {
+      domain: `${URL}/bot${BOT_TOKEN}`,
+      port: PORT,
+    },
+  });
 
   // Enable graceful stop
   process.once("SIGINT", () => bot.stop("SIGINT"));
