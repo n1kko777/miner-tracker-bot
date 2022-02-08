48464fed0fcf8c3c7af9da38295c7bdb5331b3f1
n1kko777 <3734489@gmail.com>
2022-02-08 23:31:44 +0500

    add error catcher

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const URL = process.env.URL || "";
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI;
 const BOT_TOKEN = process.env.BOT_TOKEN;
+const ADMIN_ID = process.env.ADMIN_ID;
 
 const initialize = async () => {
   const db = (
@@ -16,6 +17,11 @@ const initialize = async () => {
     })
   ).db();
   const bot = setup(db);
+
+  bot.catch((error) => {
+    console.log("error", error);
+    bot.telegram.sendMessage(ADMIN_ID, `Error executing a command: ${error}`);
+  });
 
   bot.launch(
     process.env.NODE_ENV !== "development" ?? {
