a462757c77507fdf6a3417e0f6cbe476d99874b2
n1kko777 <3734489@gmail.com>
2022-03-08 19:44:48 +0500

    no fastify

--- app/index.js
+++ app/index.js
@@ -1,20 +1,20 @@
 require("dotenv").config();
-const fastify = require("fastify");
-const path = require("path");
+// const fastify = require("fastify");
+// const path = require("path");
 
 const { MongoClient } = require("mongodb");
 const { setup } = require("./bot");
 
-const PORT = process.env.PORT;
-const WEBHOOK_URL = process.env.URL;
+// const PORT = process.env.PORT;
+// const WEBHOOK_URL = process.env.URL;
 const MONGODB_URI = process.env.MONGODB_URI;
 const ADMIN_ID = process.env.ADMIN_ID;
 
-const app = fastify();
+// const app = fastify();
 
-app.register(require("fastify-static"), {
-  root: path.join(__dirname, "./static"),
-});
+// app.register(require("fastify-static"), {
+//   root: path.join(__dirname, "./static"),
+// });
 
 const initialize = async () => {
   const db = (
@@ -26,21 +26,23 @@ const initialize = async () => {
 
   const bot = setup(db);
 
-  const SECRET_PATH = `/telegraf/${bot.secretPathComponent()}`;
-  app.post(SECRET_PATH, (req, rep) => bot.handleUpdate(req.body, rep.raw));
+  // const SECRET_PATH = `/telegraf/${bot.secretPathComponent()}`;
+  // app.post(SECRET_PATH, (req, rep) => bot.handleUpdate(req.body, rep.raw));
+
+  bot.launch();
 
   bot.catch((error) => {
     console.log("error", error);
     bot.telegram.sendMessage(ADMIN_ID, `Error executing a command: ${error}`);
   });
 
-  bot.telegram.setWebhook(WEBHOOK_URL + SECRET_PATH).then(() => {
-    console.log("Webhook is set on", WEBHOOK_URL);
-  });
+  // bot.telegram.setWebhook(WEBHOOK_URL + SECRET_PATH).then(() => {
+  //   console.log("Webhook is set on", WEBHOOK_URL);
+  // });
 
-  app.listen(PORT).then(() => {
-    console.log("Listening on port", PORT);
-  });
+  // app.listen(PORT).then(() => {
+  //   console.log("Listening on port", PORT);
+  // });
 };
 
 initialize();
