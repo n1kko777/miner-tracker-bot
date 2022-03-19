5f4fa00cf37d9dea74b79bb74cf0e7346fa06eda
Nikita Tarasov <3734489@gmail.com>
2022-03-19 12:58:58 +0500

    Revert "fix fastify telegraf"

--- app/index.js
+++ app/index.js
@@ -1,24 +1,20 @@
-// const fastify = require("fastify")({ logger: true });
-const fastify = require("fastify");
-const telegrafPlugin = require("fastify-telegraf");
-
-const path = require("path");
+require("dotenv").config();
+// const fastify = require("fastify");
+// const path = require("path");
 
 const { MongoClient } = require("mongodb");
 const { setup } = require("./bot");
 
-const PORT = process.env.PORT;
+// const PORT = process.env.PORT;
+// const WEBHOOK_URL = process.env.URL;
 const MONGODB_URI = process.env.MONGODB_URI;
 const ADMIN_ID = process.env.ADMIN_ID;
 
-const { URL: WEBHOOK_URL } = process.env;
-if (!WEBHOOK_URL) throw new Error('"URL" env var is required!');
+// const app = fastify();
 
-const app = fastify({ logger: true });
-
-app.register(require("fastify-static"), {
-  root: path.join(__dirname, "./static"),
-});
+// app.register(require("fastify-static"), {
+//   root: path.join(__dirname, "./static"),
+// });
 
 const initialize = async () => {
   const db = (
@@ -30,33 +26,23 @@ const initialize = async () => {
 
   const bot = setup(db);
 
-  const SECRET_PATH = `/telegraf/${bot.secretPathComponent()}`;
+  // const SECRET_PATH = `/telegraf/${bot.secretPathComponent()}`;
+  // app.post(SECRET_PATH, (req, rep) => bot.handleUpdate(req.body, rep.raw));
 
-  app.register(telegrafPlugin, { bot, path: SECRET_PATH });
-
-  bot.telegram.setWebhook(WEBHOOK_URL).then(() => {
-    console.log("Webhook is set on", WEBHOOK_URL);
-  });
+  bot.launch();
 
   bot.catch((error) => {
     console.log("error", error);
     bot.telegram.sendMessage(ADMIN_ID, `Error executing a command: ${error}`);
   });
 
-  bot.telegram.setWebhook(WEBHOOK_URL).then(() => {
-    console.log("Webhook is set on", WEBHOOK_URL);
-  });
+  // bot.telegram.setWebhook(WEBHOOK_URL + SECRET_PATH).then(() => {
+  //   console.log("Webhook is set on", WEBHOOK_URL);
+  // });
 
-  app.listen(PORT, "0.0.0.0").then((error) => {
-    console.log("Listening on port", PORT);
-
-    if (error) {
-      console.log("error", error);
-      bot.telegram.sendMessage(ADMIN_ID, `Error executing a command: ${error}`);
-
-      throw error;
-    }
-  });
+  // app.listen(PORT).then(() => {
+  //   console.log("Listening on port", PORT);
+  // });
 };
 
 initialize();
