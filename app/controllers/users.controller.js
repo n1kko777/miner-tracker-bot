65554c35460290fb84b6c1f8e0cb3218a24de619
n1kko777 <3734489@gmail.com>
2022-04-04 22:54:15 +0500

    CRUD subscription data api

--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -1,6 +1,10 @@
+const ObjectID = require("mongodb").ObjectID;
+
+const COLLECTION_NAME = "sessions";
+
 async function listUsers(req, reply) {
   try {
-    const users = this.mongo.db.collection("sessions");
+    const users = this.mongo.db.collection(COLLECTION_NAME);
     const result = await users.find({}).toArray();
     reply.code(200).send(result);
   } catch (error) {
@@ -8,4 +12,60 @@ async function listUsers(req, reply) {
   }
 }
 
-module.exports = { listUsers };
+async function getUser(req, reply) {
+  try {
+    const users = this.mongo.db.collection(COLLECTION_NAME);
+    const result = await users.findOne({ _id: new ObjectID(req.params.id) });
+
+    if (result) {
+      return reply.code(200).send(result);
+    }
+
+    reply.code(204).send({ message: "Not found" });
+  } catch (error) {
+    reply.code(500).send(error);
+  }
+}
+
+async function updateUser(req, reply) {
+  try {
+    const users = this.mongo.db.collection(COLLECTION_NAME);
+    const user = await users.findOne({ _id: new ObjectID(req.params.id) });
+
+    const { payer_email, tz, notification, binance, xmr } = req.body;
+    const updateDoc = {
+      $set: {
+        "data.bio.payer_email": payer_email || user.data.bio.payer_email,
+        "data.binance": binance || user.data.binance,
+        "data.xmr": xmr || user.data.xmr,
+        "data.settings.notification":
+          notification || user.data.settings.notification,
+        "data.settings.tz": tz || user.data.settings.tz,
+      },
+    };
+    const result = await users.updateOne(
+      { _id: ObjectID(req.params.id) },
+      updateDoc,
+      { upsert: true }
+    );
+    reply.send({
+      message: `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`,
+    });
+  } catch (error) {
+    reply.code(500).send(error);
+  }
+}
+
+async function deleteUser(req, reply) {
+  try {
+    const users = this.mongo.db.collection(COLLECTION_NAME);
+    const result = await users.deleteOne({ _id: ObjectID(req.params.id) });
+    if (result.deletedCount) return reply.send("Deleted");
+
+    reply.code(500).send("Could not delete.");
+  } catch (error) {
+    reply.code(500).send(error);
+  }
+}
+
+module.exports = { listUsers, getUser, updateUser, deleteUser };
--- app/routes/users.js
+++ app/routes/users.js
@@ -1,6 +1,15 @@
-const { listUsers } = require("../controllers/users.controller");
+const {
+  listUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/users.controller");
 
 async function routes(fastify, options) {
   fastify.get("/users", listUsers);
+  fastify.get("/users/:id", getUser);
+  fastify.put("/users/:id", updateUser);
+  fastify.delete("/users/:id", deleteUser);
 }
+
 module.exports = routes;
